729f632c70dc80e766fff8f69c57cd9d6925dc9f
christian-creator <christian.peder@hotmail.com>
2022-06-15 14:25:26 +0200

    New data :)

---
+++ R_new/01_load_clean.R
@@ -0,0 +1,1 @@
+load("./data/_raw/MM909_data.RData")
---
+++ R_new/02_augment.R
@@ -0,0 +1,7 @@
+library("MCPcounter")
+
+estimate_output <- myEstimateFunction(tpm_ensg)
+MCP_output <- MCPcounter.estimate(tpm_ensg,featuresType = "HUGO_symbols")
+
+
+
